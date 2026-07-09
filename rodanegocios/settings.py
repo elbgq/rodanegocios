49861e1d1938fac171eb35220fa5473ea782e99e
eloi lerner <eloibgq@gmail.com>
2026-07-09 11:31:21 -0300

    Ajuste do ALLOWED_HOSTS no settings

--- rodanegocios/settings.py
+++ rodanegocios/settings.py
@@ -32,11 +32,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    'rodanegocios.onrender.com',
+    'rodanegocios-1.onrender.com',
     '.render.com',
     'localhost',
     '127.0.0.1'
 ]
+CSRF_TRUSTED_ORIGINS = ['https://rodanegocios-1.onrender.com']
 
 
 # Application definition
